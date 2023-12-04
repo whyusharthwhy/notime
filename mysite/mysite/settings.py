ef10d150f93f85980c6c2316e004e32b9327e355
whyusharthwhy <yusharth.singh@yashdoamail.com>
2023-12-04 19:11:34 +0530

    login logout done

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+LOGIN_REDIRECT_URL = "/"
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +55,7 @@ ROOT_URLCONF = "mysite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "mysite/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView  # new
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("polls.urls")),
+    # path("", include("polls.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),  # new
+    path("accounts/", include("django.contrib.auth.urls")),
 
 ]
